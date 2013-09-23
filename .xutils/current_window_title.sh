261997af2d546c90a8130040eb84ad14a350010e
Baabelfish <baabelfish@gmail.com>
2013-09-23 11:08:45 +0300

    Who reads these anyways?

--- .xutils/current_window_title.sh
+++ .xutils/current_window_title.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo -n "$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) WM_CLASS | rev | cut -f2 -d'"' | rev) :"
-echo "$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME | cut -f2 -d'=' | tr -d '"' )"
+APP="$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) WM_CLASS | rev | cut -f2 -d'"' | rev)"
+INFO="$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME | cut -f2 -d'=' | tr -d '"' )"
+
+if [[ ! -z $APP ]]; then
+    echo "$APP :$INFO"
+fi
