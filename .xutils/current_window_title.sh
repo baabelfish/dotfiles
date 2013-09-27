b6718af5a25d09e8668eaf06fcf99fdcbb56dc3c
Baabelfish <baabelfish@gmail.com>
2013-09-27 18:16:09 +0300

    Added volemak

--- .xutils/current_window_title.sh
+++ .xutils/current_window_title.sh
@@ -2,6 +2,6 @@
 APP="$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) WM_CLASS | rev | cut -f2 -d'"' | rev)"
 INFO="$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME | cut -f2 -d'=' | tr -d '"' )"
 
-if [[ ! -z $APP ]]; then
+if [[ ! -z $APP ]] && [[ "$APP" != "WM_CLASS" ]]; then
     echo "$APP :$INFO"
 fi
--- .xutils/togglelayout.sh
+++ .xutils/togglelayout.sh
@@ -4,8 +4,8 @@ setxkbmap -option caps:escape
 
 if [[ -e ~/.xutils/.lock_colemack ]]; then
     rm ~/.xutils/.lock_colemack
-    notify-send -t 3 "Layout: colemak"
-    xmodmap ~/.xbcolemak
+    notify-send -t 3 "Layout: volemak"
+    xmodmap ~/.xvcolemak
 else
     touch ~/.xutils/.lock_colemack
     notify-send -t 3 "Layout: qwerty"
