00b11aea985202692859d4cdcef27b8812678e05
Baabelfish <baabelfish@gmail.com>
2013-09-27 18:17:31 +0300

    Who reads these anyways?

--- .xutils/togglelayout.sh
+++ .xutils/togglelayout.sh
@@ -5,7 +5,7 @@ setxkbmap -option caps:escape
 if [[ -e ~/.xutils/.lock_colemack ]]; then
     rm ~/.xutils/.lock_colemack
     notify-send -t 3 "Layout: volemak"
-    xmodmap ~/.xvcolemak
+    xmodmap ~/.xbvolemak
 else
     touch ~/.xutils/.lock_colemack
     notify-send -t 3 "Layout: qwerty"
