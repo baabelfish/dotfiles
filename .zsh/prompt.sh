44fea330145b244070342c9a278add94f42664e7
Baabelfish <baabelfish@gmail.com>
2013-10-11 08:54:35 +0300

    Maybe it's enough now

--- .zsh/prompt.sh
+++ .zsh/prompt.sh
@@ -9,18 +9,15 @@ if [[ "$PROMPT_STYLE" == "basic" ]]; then
     prompt walters
 fi
 
-aC() {
+bC() {
+    echo -ne "$BG[$1]"
+}
+
+fC() {
     echo -ne "$FG[$1]"
 }
 
 if [[ "$PROMPT_STYLE" == "fancy" ]]; then
-    # Colors
-    LC=250
-    IC=070
-    NC=076
-    WC=196
-    AC=118
-
     precmd() {
         # Show return value
         ERR=$?
@@ -33,7 +30,9 @@ if [[ "$PROMPT_STYLE" == "fancy" ]]; then
 
         # Show virtual environment
         VENV=$(echo $VIRTUAL_ENV|rev|cut -f1 -d'/'|rev)
-        [[ ! -z "$VENV" ]] && VENV="$VENV "
+        if [[ -z "$VENV" ]]; then
+            VENV="»"
+        fi
 
         # Show branch name
         d=$(pwd)
@@ -55,17 +54,37 @@ if [[ "$PROMPT_STYLE" == "fancy" ]]; then
             GBRANCH="-"
         fi
 
-PROMPT="%{$(aC $LC)%}$VENV\
-%{$(aC $IC)%}» %{$reset_color%}%b"
+SEP_LEFT="▶"
+SEP_RIGHT="◀"
+LC=250
+LBC=234
+IC=070
+NC=076
+WC=160
+AC=118
 
 RPROMPT="\
-%{$(aC $WC)%}%B$ERR\
-%{$reset_color%}%{$(aC $IC)%} « \
-%{$(aC $LC)%}%m\
-%{$reset_color%}%{$(aC $IC)%} « \
-%{$(aC $LC)%}$GBRANCH\
-%{$reset_color%}%{$(aC $IC)%} « \
-%{$(aC $LC)%}%~\
-%b%{$reset_color%}"
+%{$(bC 233)%}%{$(fC 234)%} $SEP_RIGHT\
+%{$(bC 234)%}%{$(fC $IC)%} %m\
+%{$(bC 234)%}%{$(fC 235)%} $SEP_RIGHT\
+%{$(bC 235)%}%{$(fC 082)%} %B$GBRANCH%b\
+%{$(bC 235)%}%{$(fC 236)%} $SEP_RIGHT\
+%{$(bC 236)%}%{$(fC 076)%} %~ %{$reset_color%}"
+
+PROMPT="\
+%{$(bC 235)%}%{$(fC $IC)%} %B$VENV \
+%{$(bC 233)%}%{$(fC 235)%}$SEP_LEFT\
+%{$reset_color%}%b "
+
+if [[ ! -z $ERR ]]; then
+    PROMPT="%{$(bC 236)%}%{$(fC $WC)%} %B$ERR %b\
+%{$(bC 235)%}%{$(fC 236)%}$SEP_LEFT\
+$PROMPT "
+fi
+
     }
+
+    # preexec() {
+    #     echo -ne "\033[0m"
+    # }
 fi
