f7926309bb77cda918af8f169ee30c675f1e193f
Niko Junkala <baabelfish@gmail.com>
2013-10-23 09:24:44 +0300

    Move :

--- .xutils/current_window_title.sh
+++ .xutils/current_window_title.sh
@@ -3,5 +3,5 @@ APP="$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) WM_CLAS
 INFO="$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME | cut -f2 -d'=' | tr -d '"' )"
 
 if [[ ! -z $APP ]] && [[ "$APP" != "WM_CLASS" ]]; then
-    echo "$APP :$INFO"
+    echo "$APP:$INFO"
 fi
