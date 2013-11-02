671461d245869744e88ba8a5b927c2752b90c06d
Niko Junkala <baabelfish@gmail.com>
2013-11-02 17:41:28 +0200

    Added colors and D command

---
+++ .zsh/colorcodes.sh
@@ -0,0 +1,15 @@
+# Colors.
+BLUE="[38;05;67m"
+CYAN='\033[1;36m'
+GREEN="[38;05;107m"
+ORANGE="[38;05;179m"
+PURPLE="[38;05;96m"
+RED="[38;05;167m"
+YELLOW="[38;05;222m"
+blue='\033[0;34m'
+cyan='\033[0;36m'
+default='\033[0m'
+green='\033[0;32m'
+purple='\033[0;35m'
+red='\033[0;31m'
+yellow='\033[0;33m'
--- .zsh/filemanager.sh
+++ .zsh/filemanager.sh
@@ -1,4 +1,20 @@
 #/bin/bash
+source "$HOME/.zsh/colorcodes.sh"
+BLUE="[38;05;67m"
+CYAN='\033[1;36m'
+GREEN="[38;05;107m"
+ORANGE="[38;05;179m"
+PURPLE="[38;05;96m"
+RED="[38;05;167m"
+YELLOW="[38;05;222m"
+blue='\033[0;34m'
+cyan='\033[0;36m'
+default='\033[0m'
+green='\033[0;32m'
+purple='\033[0;35m'
+red='\033[0;31m'
+yellow='\033[0;33m'
+
 IFS=$'\n'
 
 FILES=($(find *))
@@ -31,11 +47,14 @@ for (( i = 0; i < ${#UFILES[@]}; i++ )); do
     FILE=$(echo ${UFILES[$i]}|cut -f2- -d ' ')
     OLDFILE=$(echo ${OFILES[$i]}|cut -f2- -d ' ')
     case $TYPE in
-        d) echo "Deleting \"$FILE\""
+        D) echo -e "${RED}Deleting${default} $FILE"
+            rm -rf $FILE
+            ;;
+        d) echo -e "${RED}Deleting${default} $FILE"
             rm -f $FILE
             ;;
         -)  if [[ "$OLDFILE" != "$FILE" ]]; then
-                echo "Renaming \"$OLDFILE\" to \"$FILE\""
+                echo -e "${yellow}Renaming${default} $OLDFILE to $FILE"
                 mv $OLDFILE $FILE
             fi
             ;;
