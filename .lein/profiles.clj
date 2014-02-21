58ed5ab48dbc42ce0206bdd5c0a3fe12bcc61cec
Baabelfish <baabelfish@gmail.com>
2014-02-21 20:42:54 +0200

    asd

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,3 @@
-{:user
- {:dependencies [[clj-stacktrace "0.2.7"]]
-  :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                      'print-cause-trace)
-                     new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                     'pst)]
-                 (alter-var-root orig (constantly (deref new))))]}
- :plugins
- [[jonase/eastwood "0.1.0"]
-  [slamhound "1.5.1"]]}
+{:user {:plugins [[jonase/eastwood "0.1.0"]
+                  [lein-midje "3.0.0"]
+                  [slamhound "1.5.1"]]}}
