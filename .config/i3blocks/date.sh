9fc1bed92c2f6e002765297a83154591897b0bb2
Niko Junkala <niko.junkala@gofore.com>
2014-10-15 22:51:11 +0300

    asd

--- .config/i3blocks/date.sh
+++ .config/i3blocks/date.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$BLOCK_BUTTON" == 1 ]]; then
-    urxvtc -geometry 69x36 --hold -title float -e cal -m -y
+    urxvtc -geometry 78x36 --hold -title float -e cal -m -y -w
 fi
 
 date +"%a %d %b %H:%M:%S"
