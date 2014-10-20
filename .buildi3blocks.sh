f4d912d1886ec6a93be336dffc6c77eb0859c02b
baabelfish <baabelfish@gmail.com>
2014-10-20 15:47:03 +0300

    Package installation scripts and list

--- .buildi3blocks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.local/apps
-rm -rf ~/.local/apps/i3blocks
-cd ~/.local/apps
-git clone https://github.com/vivien/i3blocks
-cd i3blocks
-git checkout a1782404c7d933145b048d0d1872ea40d7a293b6
-make
