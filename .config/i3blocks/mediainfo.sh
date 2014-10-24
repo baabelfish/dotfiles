2632beb131ef4af1b113ed0307edd25207a7296b
Niko Junkala <niko.junkala@gofore.com>
2014-10-24 22:33:50 +0300

    asd

--- .config/i3blocks/mediainfo.sh
+++ .config/i3blocks/mediainfo.sh
@@ -3,10 +3,11 @@ STATUS="$(playerctl status)"
 
 if [[ $STATUS != "" ]]; then
     ARTIST="$(playerctl metadata artist)"
-    ALBUM="$(playerctl metadata album)"
-    TRACK="$(playerctl metadata title)"
+    ALBUM="$(playerctl metadata album | cut -d'(' -f1)"
+    TRACK="$(playerctl metadata title | cut -d'(' -f1)"
+    STR="$(echo "$ARTIST - $TRACK" | sed -e 's/^ *//' | sed -e 's/ *$//' | cut -c 1-50)"
 
-    echo -n "$ARTIST - $TRACK"
+    echo -n "$STR"
     if [[ $STATUS == "Playing" ]]; then
         echo " (►)"
     elif [[ $STATUS == "Paused" ]]; then
