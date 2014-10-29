acbcad1d7f61e5ebe5bc4c89615888471b737c36
Niko Junkala <niko.junkala@gofore.com>
2014-10-29 15:52:45 +0200

    asd

--- .config/i3blocks/network.sh
+++ .config/i3blocks/network.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 TOTAL=$(wicd-cli --wireless -d)
-ESSID=$(echo "$TOTAL" | grep '^Essid:' | cut -f2- -d' ')
-QUALITY=$(echo "$TOTAL" | grep '^Quality:' | cut -f2- -d' ')
-IP=$(echo "$TOTAL" | grep '^IP:' | cut -f2- -d' ')
-echo "$ESSID ($IP)"
+
+if [[ -n "$TOTAL" ]]; then
+    IP=$(echo "$TOTAL" | grep '^IP:' | cut -f2- -d' ')
+    ESSID=$(echo "$TOTAL" | grep '^Essid:' | cut -f2- -d' ')
+    QUALITY=$(echo "$TOTAL" | grep '^Quality:' | cut -f2- -d' ')
+    echo "∿ $ESSID ($IP)"
+else
+    TOTAL=$(wicd-cli --wired -d)
+    echo "- $IP"
+fi
+
