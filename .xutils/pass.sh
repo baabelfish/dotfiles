e85e9803608fd9e862c2ac8b55c2e2d0d1ed5f0b
Baabelfish <baabelfish@gmail.com>
2015-09-07 15:04:31 +0300

    asd

--- .xutils/pass.sh
+++ .xutils/pass.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pass --clip $(pass ls | tail -n +2 | rev | cut -f1 -d' ' | rev | dmenu -i -b -p "Pass:") && killall gpg-agent
+pass --clip $(pass ls | tail -n +2 | rev | cut -f1 -d' ' | rev | rofi -dmenu -i -p "Pass:") && killall gpg-agent
